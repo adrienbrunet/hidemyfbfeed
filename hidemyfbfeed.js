650039eaf079c1a46e56f2c491ee0cc505749096
Adrien Brunet <adrien@emencia.com>
2018-07-05 20:43:08 +0200

    :art: hidemyfbfeed.js file

---
+++ hidemyfbfeed.js
@@ -0,0 +1,9 @@
+/* Hide newsfeed */
+let topnews = document.getElementById("topnews_main_stream_408239535924329");
+topnews.style.visibility = "hidden";
+
+/* Remove trending stories and suggested groups */
+let pagelet_trending_tags_and_topics = document.getElementById("pagelet_trending_tags_and_topics");
+let pagelet_ego_pane = document.getElementById("pagelet_ego_pane");
+pagelet_trending_tags_and_topics.style.visibility = "hidden";
+pagelet_ego_pane.style.visibility = "hidden";
